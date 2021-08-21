6a1246acd172d7aa6d277708b2efa3775434490c
Guido Rapisarda <12972491+guidorapisarda@users.noreply.github.com>
2021-08-21 14:16:33 -0300

    cambios menores

--- src/App.js
+++ src/App.js
@@ -8,36 +8,9 @@ import {BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-do
  function App() {
   return (
     <>
-      <Router>
-        <NavBar/>
-        <Switch>
-          <Route exact path="/">
-            <ItemListContainer/>
-          </Route>
-          <Route exact path="/products">
-            <hr/>
-            <ItemListContainer/>
-            <hr/>
-          </Route>
-          <Route exact path="/products/:catId">
-            
-            <hr/>
-            <ItemListContainer/>
-            <hr/>
-          </Route>
-          <Route path="/clicktracker">
-            <ClickTracker/>
-          </Route>
-          <Route path="/nosotros">
-            <h2>Info sobre nosotros</h2>
-          </Route>
-          <Route path="/cart">
-          </Route>
-          <Route path="*">
-            <Redirect to="/"/>
-          </Route>
-        </Switch>
-      </Router>
+      <NavBar/>
+      <hr/>
+      <ItemListContainer greeting="Aqui existira el listado de productos"/>
     </>
   );
 }
