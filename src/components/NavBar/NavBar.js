9173978184d3e847b5d9e64f25d865d0a212748e
Guido Rapisarda <12972491+guidorapisarda@users.noreply.github.com>
2021-08-21 14:15:41 -0300

    cambios menores

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,49 +1,18 @@
 import React from 'react';
 import {CartWidget} from './CartWidget/CartWidget';
-import {Link} from 'react-router-dom'
-import {Navbar,Nav,NavDropdown,Container} from 'react-bootstrap'
 
 export const NavBar = () => {
     return (
     <>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
-            </Nav>
-            <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-            </Nav.Link>
-            </Nav>
-        </Navbar.Collapse>
-        </Container>
-        </Navbar>
         <nav>
-            <Link to="/">
-                <div>
-                    <h1>ecommerce con React!</h1>
-                </div>
-            </Link>
-            <Link to="/products">
-                <h2>productos</h2>
-            </Link>
-            <Link to="/cart">
-                <h2>Cart</h2>
+            <div>
+                <h1>ecommerce con React!</h1>
+            </div>
+            <div>
+                <a href="#">Home</a>
+                <a href="#">Productos</a>
                 <CartWidget/>
-            </Link>
+            </div>
         </nav>
     </>
     );
